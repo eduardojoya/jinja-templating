bd8320685091f14c58ace523a958f0fa10ebf625
eduardojoya <ebjoya@gmail.com>
2019-03-01 15:54:03 -0800

    finished Phase 3 and modified base.html template

--- manage.py
+++ manage.py
@@ -11,33 +11,36 @@ def content_files(): #auto-generates the list
         pages.append({ #add to list 
         'filename': 'content/' + name_only + extension, 
         'title': name_only.title(), 
-        'output': 'docs/' + name_only + extension
-#        'output_filename': name_only + extension 
+        'output': 'docs/' + name_only + extension,
+        'links': name_only + extension
     })
     return pages 
 
 print('Phase 2 - Jinja2')
-def apply_template(template_html, file_name, page_title):
+def apply_template(template_html, file_name, page_title,page_links):
     from jinja2 import Template
+    pages = content_files()
     content_html = open(file_name).read()
     template_html = open('templates/base.html').read()
     template = Template(template_html)
     return template.render(
     title= page_title, #page_title taken from the list from the content_files function 
     content=content_html, #content_html from the files in pages list 
+    navigation_links=page_links,
     pages = pages)
+
+print('Phase 2 & 3 Complete!')
     
 def output_page(template_html,page):
     file_name = page['filename']
     page_output = page['output']
     page_title = page['title']
+    page_links = page['links']
     
-
-    output_html = apply_template(template_html, file_name, page_title)
+    output_html = apply_template(template_html, file_name, page_title, page_links)
     open(page_output, 'w+').write(output_html)        
-    
-print('Phase 2 Complete!')
 
+    
 def main():
     pages = content_files()
     template_html = open('templates/base.html').read() 
